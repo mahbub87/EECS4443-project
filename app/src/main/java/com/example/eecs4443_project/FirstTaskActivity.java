5b95d8d31b76d30910424b2be7504aeadc91a306
Lhenea <zetasovery@gmail.com>
2025-03-30 22:50:13 -0400

    added speech to text in first activity

--- app/src/main/java/com/example/eecs4443_project/FirstTaskActivity.java
+++ app/src/main/java/com/example/eecs4443_project/FirstTaskActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -15,6 +17,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class FirstTaskActivity extends AppCompatActivity {
     private int VOICE_PERMISSION = 100;
 
+    private TextView textView;
+    private Button speak;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +29,16 @@ public class FirstTaskActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textView = findViewById(R.id.textViewTEST);
+        speak = findViewById(R.id.buttonTest);
+
+        speak.setOnClickListener(v ->{
+            speak();
+        });
     }
 
-    public void speak(View view){
+    public void speak(){
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start Speak");
@@ -39,7 +50,7 @@ public class FirstTaskActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode == VOICE_PERMISSION && resultCode == RESULT_OK){
-            //textview.settext(data.getStringArrayListExtra(RecorgnizerIntent.EXTRA_RESULTS).get(0);
+            textView.setText(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0));
 
         }
     }
