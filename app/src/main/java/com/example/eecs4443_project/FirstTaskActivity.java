39b3214f6e273622bb41bff7cde8b78d47861f45
Lhenea <zetasovery@gmail.com>
2025-03-31 22:07:33 -0400

    first task activation code logic, time and error results logic

--- app/src/main/java/com/example/eecs4443_project/FirstTaskActivity.java
+++ app/src/main/java/com/example/eecs4443_project/FirstTaskActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,15 +17,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.apache.commons.lang3.time.StopWatch;
+
 import java.util.ArrayList;
 
 public class FirstTaskActivity extends AppCompatActivity {
     private static final int VOICE_PERMISSION = 100;
 
 
+    private ImageView check, check2;
     private TextView textView;
-    Stopwatch
-    private Button speak, confirm, tryagain;
+    StopWatch stopWatch = new StopWatch();
+    private int tryagain, errorPercent, errorPercent2,  tryagain2;
+    private long time, time2;
+    private String activationWord = "Hey Guide!";
+    private Button confirm, confirm2, next;
+    private ImageButton speak, speak2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,14 +46,57 @@ public class FirstTaskActivity extends AppCompatActivity {
 
         //initializes
         textView = findViewById(R.id.textViewTEST);
-        speak = findViewById(R.id.buttonTest);
+        speak = findViewById(R.id.fs1_speak_button);
+        speak2 = findViewById(R.id.fs2_speak_button);
+        confirm = findViewById(R.id.fs1_confirm_button);
+        confirm2 = findViewById(R.id.fs2_confirm_button);
+        next = findViewById(R.id.f_next_button);
+        tryagain = 0;
+        tryagain2 = 0;
+        errorPercent = 0;
+        errorPercent2 = 0;
+        time = 0;
+        time2 = 0;
+
+        //turn the buttons invisible first
+        //myButton.setVisibility(View.VISIBLE);
+        confirm.setVisibility(View.INVISIBLE);
+        confirm2.setVisibility(View.INVISIBLE);
+        check.setVisibility(View.INVISIBLE);
+        check2.setVisibility(View.INVISIBLE);
 
         //when clicked opens microphone intent
         speak.setOnClickListener(v ->{
+            tryagain++; // tracks button click
             speak();
+            stopWatch.reset();
+            stopWatch.start();
+            confirm.setVisibility(View.VISIBLE);
         });
 
-        //
+        speak2.setOnClickListener(v ->{
+            activationWord = "weather";
+            tryagain2++; // tracks button click
+            speak();
+            stopWatch.reset();
+            stopWatch.start();
+            confirm2.setVisibility(View.VISIBLE);
+        });
+
+        //if next button clicked
+        next.setOnClickListener(v -> {
+            if ((tryagain != 0 && tryagain2 != 0)){
+                Intent next = new Intent(this, ResultsActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putLong("time", time);
+                bundle.putLong("time2", time2);
+                bundle.putInt("error", errorPercent);
+                bundle.putInt("error2", errorPercent2);
+                next.putExtras(bundle);
+                startActivity(next);
+                finish();
+            }
+        });
     }
 
     /**
@@ -81,12 +133,53 @@ public class FirstTaskActivity extends AppCompatActivity {
                 String query = results.get(0);
                 //do a dialogue pop up here
                 textView.setText("You said: " + query);
-                //two buttons, confirm or try again
+                withActivationWord(query);
 
-                searchWeather(query);
+                //confirm button if clicked, stopwtch stopped and method called
+                confirm.setOnClickListener(v -> {
+                    errorPercent = errorPercentage(tryagain);
+                    stopWatch.stop();
+                    time = stopWatch.getTime();
+                    check.setVisibility(View.INVISIBLE);
+                });
+
+                confirm2.setOnClickListener(v -> {
+                    errorPercent2 = errorPercentage(tryagain2);
+                    stopWatch.stop();
+                    time2 = stopWatch.getTime();
+                    check2.setVisibility(View.INVISIBLE);
+                });
+
             }
 
         }
+    }
+
+    /**
+     * method to check with the activation word
+     * @param query
+     */
+    private void withActivationWord(String query) {
+        // Check if the query contains the activation word
+        if (query.toLowerCase().contains(activationWord)) {
+          //open web browser
+            int startIndex = query.indexOf(activationWord) + activationWord.length();
+
+            // Extract everything after the activation word
+            String command = query.substring(startIndex).trim();
+            searchWeather(command);
+        }
+    }
+
+    /**
+     * method that calculates error percentage
+     * @param tries
+     * @return
+     */
+    private int errorPercentage(int tries){
+        int errors = tries - 1;
+        int percent = (errors/tries) * 100;
+        return percent;
     }
 
     /**
